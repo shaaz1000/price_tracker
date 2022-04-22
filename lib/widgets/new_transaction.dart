56dff8308a581b01ae428f2c2fbf6368bd1ab62a
shaaz-pragmakw <shaazkhan153@gmail.com>
2022-04-23 04:43:10 +0530

    shs

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -7,6 +7,13 @@ class NewTransaction extends StatelessWidget {
   final Function addTransaction;
   NewTransaction(this.addTransaction);
 
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+    if (enteredTitle.isEmpty || enteredAmount <= 0) return;
+    addTransaction(enteredTitle, enteredAmount);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -20,18 +27,16 @@ class NewTransaction extends StatelessWidget {
                 autocorrect: false,
                 decoration: InputDecoration(labelText: "Title"),
                 controller: titleController,
+                onSubmitted: (_) => submitData(),
               ),
               TextField(
-                keyboardType: TextInputType.number,
+                keyboardType:
+                    const TextInputType.numberWithOptions(decimal: true),
                 decoration: InputDecoration(labelText: "Amount"),
                 controller: amountController,
+                onSubmitted: (_) => submitData(),
               ),
-              TextButton(
-                  onPressed: () {
-                    addTransaction(titleController.text,
-                        double.parse(amountController.text));
-                  },
-                  child: Text("Add Transaction"))
+              TextButton(onPressed: submitData, child: Text("Add Transaction"))
             ],
           )),
     );
